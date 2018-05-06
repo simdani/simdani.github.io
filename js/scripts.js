32705a0eca92585a79956f7c1fb6b95085ad8e00
Simonas Daniliauskas <daniliauskas.s@gmail.com>
2018-05-06 12:35:59 +0300

    change particles, text

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@
 window.onload = function() {
   Particles.init({
     selector: '.background',
-    color: ['#DA0463', '#404B69', '#DBEDF3'],
+    color: ['#0066cc', '#404B69', '#DBEDF3'],
     connectParticles: true
   });
 };
